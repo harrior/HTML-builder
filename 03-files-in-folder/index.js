d5e1d0bc707ded10efc57808bd28e4a3cc49786e
Harrior <harrior@users.noreply.github.com>
2021-11-01 16:45:41 +0300

    feat: complete third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = path.join(__dirname, 'secret-folder');
+
+
+fs.promises.readdir(dir, {withFileTypes: true}).then(files => {
+  for (let file of files) {
+    if (file.isFile()) {
+      printFileInfo(file.name).then().catch();
+    }
+  }
+});
+
+async function printFileInfo(file) {
+  let filePath = path.join(dir, file);
+  let filename = path.basename(file, path.extname(file));
+  let extension = path.extname(file).slice(1);
+
+  await fs.stat(filePath, (error, data) => {
+    const size = Math.ceil(data['size'] / 1024);
+    console.log(`${filename} - ${extension} - ${size}kb`);
+  });
+}
