49995a61411e09e9a15ed3ab81f5269fb14fb353
Harrior <harrior@users.noreply.github.com>
2021-11-02 20:42:53 +0300

    feat: complete fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+const srcPath = path.join(__dirname, 'files');
+const dstPath = path.join(__dirname, 'files-copy');
+
+fs.promises.rm(dstPath, {force: true, recursive: true}).then(() => {
+  backupDir('');
+});
+
+function backupDir(relPath) {
+  const fullSrcPath = path.join(srcPath, relPath);
+  const fullDstPath = path.join(dstPath, relPath);
+  fs.promises.mkdir(fullDstPath).then(() => {
+    fs.promises.readdir(fullSrcPath, {withFileTypes: true}).then(fileList => {
+      for (let file of fileList) {
+        if (file.isFile()) {
+          const srcFilePath = path.join(fullSrcPath, file.name);
+          const dstFilePath = path.join(fullDstPath, file.name);
+          fs.promises.copyFile(srcFilePath, dstFilePath).catch(err => console.log(err));
+        } else if (file.isDirectory()) {
+          backupDir(path.join(relPath, file.name));
+        }
+      }
+    });
+  });
+}
