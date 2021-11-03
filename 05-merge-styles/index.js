c02571f4904d18d713f4f0689610ec7ca48f0524
Harrior <harrior@users.noreply.github.com>
2021-11-03 13:05:12 +0300

    refactor: refactor fifth task with Promises

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,15 +2,24 @@ const fs = require('fs');
 const path = require('path');
 
 const stylesDir = path.join(__dirname, 'styles');
-console.log(stylesDir);
+const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
 
 fs.promises.readdir(stylesDir, {withFileTypes: true})
-  .then(files => {
-    let bundleFile = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
-    for (let file of files){
-      if (file.isFile() && (path.extname(file.name) === '.css')){
-        let srcCSS = fs.createReadStream(path.join(stylesDir, file.name));
-        srcCSS.pipe(bundleFile);
-      }
-    }
+  .then(files => files
+    .filter(file => file.isFile() && (path.extname(file.name) === '.css'))
+    .map(file => path.join(stylesDir, file.name)))
+  .then(files => Promise.all(files.map(filename => readStyles(filename))))
+  .then(results => results.flat().join('\n'))
+  .then(styles => {
+    const ws = fs.createWriteStream(bundlePath);
+    ws.write(styles);
   });
+
+function readStyles(filename) {
+  return new Promise(resolver => {
+    const rs = fs.createReadStream(filename);
+    const styles = [];
+    rs.on('data', data => styles.push(data.toString()));
+    return rs.on('end', () => resolver(styles));
+  });
+}
